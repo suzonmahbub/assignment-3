68dac834c59b3ea2c35bee8e788a8c599be9ec22
suzon89 <bdsuzonmahbub@gmail.com>
2025-08-09 22:15:12 +0600

    final js update

--- final.js
+++ final.js
@@ -42,34 +42,11 @@ if (
 }
 
 /** Problem 05 - ( PH Email Generator )  */
-var student = { name: "jhankar", roll: 1014, department: "cse" };
+var student= { name: "suzon" , roll: 1014 ,department: "cse" };
 //write your code here
 
-if (
-  student.name.toLowerCase() === "jhankar" &&
-  student.roll === 1014 &&
-  student.department.toLowerCase() === "cse"
-) {
-  console.log(
-    student.name + student.roll + "." + student.department + "@ph.ac.bd"
-  );
-} else if (
-  student.name.toLowerCase() === "monu" &&
-  student.roll === 99 &&
-  student.department.toLowerCase() === "eee"
-) {
-  console.log(
-    student.name + student.roll + "." + student.department + "@ph.ac.bd"
-  );
-} else if (
-  student.name.toLowerCase() === "mewo" &&
-  student.roll === 96 &&
-  student.department.toLowerCase() === "cse"
-) {
-  console.log(
-    student.name + student.roll + "." + student.department + "@ph.ac.bd"
-  );
-}
+console.log(student.name + student.roll + "." + student.department + "@ph.ac.bd")
+
 
 /** Problem 06 :  (Current Salary )  */
 var experience = 30;
