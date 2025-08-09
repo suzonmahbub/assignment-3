62e2891d9272a93f8d6032d79afea7817ee6076f
suzon89 <bdsuzonmahbub@gmail.com>
2025-08-09 22:00:19 +0600

    problem-5 update

--- problem-5.js
+++ problem-5.js
@@ -1,29 +1,5 @@
 /** Problem 05 - ( PH Email Generator )  */
-var student = { name: "mewo", roll: 96, department: "cse" };
+var student= { name: "suzon" , roll: 1014 ,department: "cse" };
 //write your code here
 
-if (
-  student.name.toLowerCase() === "jhankar" &&
-  student.roll === 1014 &&
-  student.department.toLowerCase() === "cse"
-) {
-  console.log(
-    student.name + student.roll + "." + student.department + "@ph.ac.bd"
-  );
-} else if (
-  student.name.toLowerCase() === "monu" &&
-  student.roll === 99 &&
-  student.department.toLowerCase() === "eee"
-) {
-  console.log(
-    student.name + student.roll + "." + student.department + "@ph.ac.bd"
-  );
-} else if (
-  student.name.toLowerCase() === "mewo" &&
-  student.roll === 96 &&
-  student.department.toLowerCase() === "cse"
-) {
-  console.log(
-    student.name + student.roll + "." + student.department + "@ph.ac.bd"
-  );
-}
+console.log(student.name + student.roll + "." + student.department + "@ph.ac.bd")
